5bd8c696c43f15d39dfd8c921ea0e34d98884f50
raregas <raregas@NGXLASER>
2020-01-13 10:27:10 -0600

    repo updated for analysis a-01-N-150

---
+++ scripts/ngx/ngx_AirShot_withGP50_withLaser.py
@@ -0,0 +1,54 @@
+#===============================================================================
+# EXTRACTION SCRIPT ngx_AirShot_withGP50_withLaser.py
+#===============================================================================
+
+'''
+modifier: 01
+eqtime: 12
+'''
+
+def main():
+
+    info('NGX Air Shot')
+
+    gosub('util:PrepareForAirShotLaser')
+
+    gosub('util:AirShot_withGP50_withLaser')
+    
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT ngx_pump_extraction_line.py
+#===============================================================================
+
+
+def main():
+
+    info('Pump after equilibration')
+    
+    close(description='MS In')
+    close(description='Laser Pump')
+    close(description='Ion Pump')
+    open(description='Turbo')
+    
+    sleep(2)
+    
+    open(description='Hex In')
+    open(description='Cryo')
+    open(description='Getter')
+    open(description='Air Port')
+
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT ngx_pump_ms.py
+#===============================================================================
+def main():
+
+    info('Pumping spectrometer')
+
+    open(description='MS Ion Pump')
+    
+    open(description='Ion Pump')
+    
+    if delay_after>0:
+        sleep(delay_after)
+    
