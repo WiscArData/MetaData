7fffd14b9a0e29d12e09705a0b1a57239e216d0a
raregas <raregas@NGXLASER>
2020-02-26 12:20:43 -0600

    repo updated for analysis ba-01-N-887

---
+++ scripts/ngx/ngx_AirShot_H3.py
@@ -0,0 +1,56 @@
+#===============================================================================
+# EXTRACTION SCRIPT ngx_AirShot_H3.py
+#===============================================================================
+
+'''
+modifier: 01
+eqtime: 8
+'''
+
+def main():
+
+    info('NGX Air Shot')
+
+    gosub('util:PrepareForAirShot')
+
+    gosub('util:AirShot_cryo_verysmall_withLaser')
+    
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT ngx_pump_extraction_line.py
+#===============================================================================
+
+
+def main():
+
+    info('Pump after equilibration')
+    
+    close(description='MS In')
+    close(description='Laser Pump')
+    close(description='Ion Pump')
+    open(description='Turbo')
+    
+    sleep(2)
+    
+    open(description='Hex In')
+    open(description='Cryo')
+    open(description='Getter')
+    open(description='Air Port')
+    open(description='Laser Port')
+    open(description='Laser Pump')
+
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT ngx_pump_ms.py
+#===============================================================================
+def main():
+
+    info('Pumping spectrometer')
+
+    open(description='MS Ion Pump')
+    
+    open(description='Ion Pump')
+    
+    #if delay_after>0:
+    #    sleep(delay_after)
+    
