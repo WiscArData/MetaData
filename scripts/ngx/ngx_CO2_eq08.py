42808d60861246a0a90aed1c2a2e3432c461767d
raregas <raregas@NGXLASER>
2020-10-15 20:04:57 -0500

    repo updated for analysis ba-01-N-2391

---
+++ scripts/ngx/ngx_CO2_eq08.py
@@ -0,0 +1,58 @@
+#===============================================================================
+# EXTRACTION SCRIPT ngx_CO2_eq08.py
+#===============================================================================
+
+'''
+modifier: 01
+eqtime: 8
+'''
+
+def main():
+
+    info('NGX CO2 laser analysis')
+
+    gosub('util:PrepareForCO2Analysis')
+
+    gosub('util:CO2Analysis')
+    
+
+    
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT ngx_pump_extraction_line.py
+#===============================================================================
+
+
+def main():
+
+    info('Pump after equilibration')
+    
+    close(description='MS In')
+    close(description='Laser Pump')
+    close(description='Ion Pump')
+    open(description='Turbo')
+    
+    sleep(2)
+    
+    open(description='Hex In')
+    open(description='Cryo')
+    open(description='Getter')
+    open(description='Air Port')
+    open(description='Laser Port')
+    open(description='Laser Pump')
+
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT ngx_pump_ms.py
+#===============================================================================
+def main():
+
+    info('Pumping spectrometer')
+
+    open(description='MS Ion Pump')
+    
+    open(description='Ion Pump')
+    
+    #if delay_after>0:
+    #    sleep(delay_after)
+    
