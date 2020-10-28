02a4a818b489b3f9af27a63eb81c0d090fb59ee4
raregas <raregas@NGXLASER>
2020-10-28 10:43:01 -0500

    repo updated for analysis a-01-N-3454

--- scripts/ngx/ngx_unknown200_30_1_H4_L4.py
+++ scripts/ngx/ngx_unknown200_30_1_H4_L4.py
@@ -87,8 +87,9 @@ def main():
         baselines(ncounts=mx.baseline.counts, integration_time=mx.baseline.integration_time, mass=mx.baseline.mass, detector=mx.baseline.detector, settling_time=mx.baseline.settling_time)
     if mx.peakcenter.after:
         activate_detectors(*mx.peakcenter.detectors, **{'peak_center':True})
-        peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope,
-        integration_time=mx.peakcenter.integration_time) 
-       
+        peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope, 
+                    integration_time=mx.peakcenter.integration_time,
+                    config_name='H440_L436') 
+
     info('finished measure script')
     
