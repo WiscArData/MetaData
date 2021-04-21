64c2aa86498a781519d3d7931efc8e226ab6222a
raregas <raregas@NGXLASER>
2021-04-21 16:48:02 -0500

    repo updated for analysis ba-01-N-2945

--- scripts/ngx/ngx_unknown1200_1200_100_H4_L4_nocenter.py
+++ scripts/ngx/ngx_unknown1200_1200_100_H4_L4_nocenter.py
@@ -10,7 +10,7 @@ baseline:
   before: false
   counts: 12
   detector: H4
-  mass: 37.75
+  mass: 37.60
   settling_time: 20.0
   integration_time: 100.0
 default_fits: nominal
@@ -76,7 +76,7 @@ def main():
 
     set_time_zero()
     
-    sniff(eqt)    
+    sleep(eqt)    
     set_fits()
     set_baseline_fits()
 
